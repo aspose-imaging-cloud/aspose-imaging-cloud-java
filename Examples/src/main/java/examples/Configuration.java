8099216d521e43379b3dab77dd86371443792550
SOHAIL <msohailismail@gmail.com>
2019-04-24 09:43:52 +0500

    Removed Deprecated Examples

--- Examples/src/main/java/examples/Configuration.java
+++
@@ -1,6 +0,0 @@
-package examples;
-
-public class Configuration {
-	public static String app_sid="78946fb4-3bd4-4d3e-b309-f9e2ff9ac6f9";
-	public static String app_key="b125f13bf6b76ed81ee990142d841195";
-}	
